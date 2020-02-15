537d0b283ac3d1ef94ec1d9cbd84b597ab419619
Jad Nohra <jadnohra@gmail.com>
2020-02-15 16:19:16 +0100

    working on besteffort simulator

--- README.md
+++ README.md
@@ -11,15 +11,52 @@ pinged
 ponged
 pinged
 ponged
- === perfect ===
- === inverted ===
-starving by 1
- === random ===
-starving by 1
-overdriven by 1
-starving by 1
-starving by 1
-overdriven by 1
-starving by 1
-starving by 1
+=== perfect ===
+* 0 OK-LAT(0)
+* 1 OK-LAT(0)
+* 2 OK-LAT(0)
+* 3 OK-LAT(0)
+* 4 OK-LAT(0)
+=== inverted ===
+* 0 STARVING-LAT(N/A)
+* 1 OK-LAT(-1)
+* 2 OK-LAT(-1)
+* 3 OK-LAT(-1)
+* 4 OK-LAT(-1)
+=== nondet-impulse ===
+* 0 STARVING-LAT(N/A)
+* 1 OK-LAT(-1)
+* 2 DROPPING(1)-LAT(0)
+* 3 STARVING-LAT(N/A)
+* 4 OK-LAT(-1)
+* 5 OK-LAT(-1)
+* 6 OK-LAT(-1)
+* 7 OK-LAT(-1)
+* 8 OK-LAT(-1)
+* 9 DROPPING(1)-LAT(0)
+=== starving ===
+* 0 STARVING-LAT(N/A)
+* 1 STARVING-LAT(N/A)
+* 2 STARVING-LAT(N/A)
+* 3 OK-LAT(0)
+* 4 STARVING-LAT(N/A)
+* 5 STARVING-LAT(N/A)
+* 6 STARVING-LAT(N/A)
+* 7 OK-LAT(0)
+* 8 STARVING-LAT(N/A)
+* 9 STARVING-LAT(N/A)
+=== dropping ===
+* 0 OK-LAT(0)
+* 3 DROPPING(2)-LAT(0)
+* 6 DROPPING(2)-LAT(0)
+* 9 DROPPING(2)-LAT(0)
+=== fuzzy ===
+* 0 STARVING-LAT(N/A)
+* 4 OK-LAT(-4)
+* 8 OK-LAT(-1)
+* 12 OK-LAT(-1)
+* 16 STARVING-LAT(N/A)
+* 20 OK-LAT(-2)
+* 24 DROPPING(1)-LAT(-1)
+* 28 OK-LAT(-2)
 ```
--- pacos/discrete_event_ism_besteffort.py
+++ pacos/discrete_event_ism_besteffort.py
@@ -102,8 +102,8 @@ def run_besteffort_starving():
                             0, 0)
     data = PeriodicImpulse(lambda _: Message(actor.in_data_pin, 'data'), 3, 3)
     engine.add_actor(actor)
-    engine.add_impulse(timer)
     engine.add_impulse(data)
+    engine.add_impulse(timer)
     engine.run(max_frames=10)
 
 
@@ -114,34 +114,34 @@ def run_besteffort_dropping():
                             2, 0)
     data = PeriodicImpulse(lambda _: Message(actor.in_data_pin, 'data'), 0, 0)
     engine.add_actor(actor)
-    engine.add_impulse(timer)
     engine.add_impulse(data)
+    engine.add_impulse(timer)
     engine.run(max_frames=10)
 
 
-def run_besteffort_random():
-    engine = IsmEngine()
+def run_besteffort_fuzzy():
+    engine = IsmEngine(impulse_rand = random.Random(0))
     actor = BestEffortActor()
     timer = PeriodicImpulse(lambda _: Message(actor.in_trigger_pin, 'timer'),
-                            0, 0)
+                            3, 0)
     data = FuzzyPeriodicImpulse(lambda _: Message(actor.in_data_pin, 'data'),
                                 3, 0, 3, random.Random(0))
     engine.add_actor(actor)
-    engine.add_impulse(timer)
     engine.add_impulse(data)
-    engine.run(max_frames=20)
+    engine.add_impulse(timer)
+    engine.run(max_frames=30)
 
 
 def run_besteffort():
     print('=== perfect ===')
-    #run_besteffort_perfect()
+    run_besteffort_perfect()
     print('=== inverted ===')
-    #run_besteffort_inverted()
+    run_besteffort_inverted()
     print('=== nondet-impulse ===')
     run_besteffort_nondet_impulse()
     print('=== starving ===')
-    #run_besteffort_starving()
+    run_besteffort_starving()
     print('=== dropping ===')
-    #run_besteffort_dropping()
-    print('=== random ===')
-    #run_besteffort_random()
+    run_besteffort_dropping()
+    print('=== fuzzy ===')
+    run_besteffort_fuzzy()
