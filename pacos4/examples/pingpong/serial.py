ffc02a213e75f376011470951b16accb4e8fbd96
Jad Nohra <jadnohra@gmail.com>
2020-02-29 14:00:52 +0100

    pacos4

--- pacos4/examples/pingpong/serial.py
+++ pacos4/examples/pingpong/serial.py
@@ -22,9 +22,8 @@ class PingTriggerProc(Procedure):
 
     def call(self, arg: CallArg, _, proxor: IProcessorAPI) -> CallResult:
         if self._actor._pings_left > 0:
-            logging.warning('step: {}, pings_left: {}'
-                            .format(proxor.step_count, 
-                                    self._actor._pings_left))
+            logging.warning('step: {}, pings_left: {}'.format(
+                             proxor.step_count, self._actor._pings_left))
             self._actor._pings_left = self._actor._pings_left - 1
             return CallResult(1, [self.create_call()])
         return CallResult()
