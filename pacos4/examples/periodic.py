21eb7acb7c771a018a1d934fbbf40bda2315dd73
Jad Nohra <jad.nohra@aid-driving.eu>
2020-03-02 09:53:49 +0100

    Adding data race examples to pacos4

---
+++ pacos4/examples/periodic.py
@@ -0,0 +1,97 @@
+import sys
+from typing import List, Any
+import logging
+import argparse
+import time
+import timeit
+import random
+from pacos4.token import Address, Token
+from pacos4.call import CallArg, Call, CallResult
+from pacos4.procedure import Procedure, IProcessorAPI
+from pacos4.actor import Actor
+from pacos4.processor import Processor, ProcessorConfig
+from pacos4.time import repr_time
+from pacos4.board import Board
+
+
+class SourceActor(Actor):
+    def __init__(self):
+        super().__init__('source', [FeedProc()])
+
+
+class FeedProc(Procedure):
+    def __init__(self):
+        super().__init__('feed')
+
+    def call(self, arg: CallArg, __, proxor: IProcessorAPI) -> CallResult:
+        return CallResult(1, 
+                          [Call(arg+1, Address(actor='source', proc='feed')),
+                           Call(arg, Address(processor='B', actor='sink'))])
+
+
+class SinkActor(Actor):
+    def __init__(self):
+        super().__init__('sink', [ConsumeProc()])
+
+
+class ConsumeProc(Procedure):
+    def __init__(self):
+        super().__init__('consume')
+
+    def call(self, arg: CallArg, __, proxor: IProcessorAPI) -> CallResult:
+        logging.warning('Sink received value: {}'.format(arg))
+        if arg < 3:
+            return CallResult()
+        return proxor.exit()
+
+
+def source_main(processor: Processor) -> None:
+    processor.add_actor(SourceActor())
+    processor.put_calls([Call(0, Address(actor='source'))])
+
+def sink_main(processor: Processor) -> None:
+    processor.add_actor(SinkActor())
+
+def create_board(log_lvl: str = 'WARNING') -> Board:
+    processor_configs = [
+        ProcessorConfig(name='A', main=source_main, log_level=log_lvl), 
+        ProcessorConfig(name='B', main=sink_main, log_level=log_lvl)
+        ]
+    return Board(processor_configs)
+
+
+def run(log_lvl: str = 'WARNING'):
+    print('=== periodic ===')
+    board = create_board(log_lvl)
+    while not board.any_exited():
+        board.step()
+    board.exit()
+
+
+def description() -> str:
+    return \
+    """
+    A periodic source.
+    """
+
+
+def process_args() -> Any:
+    parser = argparse.ArgumentParser(add_help=False)
+    parser.add_argument("--log", default='WARNING')
+    parser.add_argument('-h', '--help', action='help', 
+                        default=argparse.SUPPRESS,
+                        help=description())
+    args = parser.parse_args(sys.argv[1:])
+    logging.basicConfig(format='%(levelname)s:%(message)s',
+                        level=logging.getLevelName(args.log.upper()))
+    random.seed(time.time())
+    return args
+
+
+def main():
+    args = process_args()
+    run(args.log)
+
+
+if __name__ == "__main__":
+    main()
---
+++ pacos4/examples/race/compute_race.py
@@ -0,0 +1,161 @@
+import sys
+from typing import List, Any
+import logging
+import argparse
+import time
+import timeit
+import random
+from pacos4.token import Address, Token
+from pacos4.call import CallArg, Call, CallResult
+from pacos4.procedure import Procedure, IProcessorAPI
+from pacos4.actor import Actor
+from pacos4.processor import Processor, ProcessorConfig
+from pacos4.time import repr_time
+from pacos4.board import Board
+
+
+class SourceActor(Actor):
+    def __init__(self):
+        super().__init__('source', [FeedProc()])
+
+
+class FeedProc(Procedure):
+    def __init__(self):
+        super().__init__('feed')
+
+    def call(self, arg: CallArg, __, proxor: IProcessorAPI) -> CallResult:
+        my_step_count = 1
+        return CallResult(my_step_count, 
+                          [Call(arg+1, Address(actor='source', proc='feed'), 
+                                call_step=proxor.step_count+my_step_count+2),
+                           Call(arg, Address(processor='C', actor='compute', 
+                                             proc='data1'))])
+
+
+class DoubleSourceActor(Actor):
+    def __init__(self):
+        super().__init__('source', [DoubleFeedProc()])
+
+
+class DoubleFeedProc(Procedure):
+    def __init__(self):
+        super().__init__('feed')
+
+    def call(self, arg: CallArg, __, proxor: IProcessorAPI) -> CallResult:
+        return CallResult(1, 
+                          [Call(None, Address(actor='source', proc='feed')),
+                           Call(1, Address(processor='C', actor='compute',
+                                           proc='data2'))])
+
+
+class ComputeActor(Actor):
+    def __init__(self):
+        super().__init__('compute', [Data1Proc(self), Data2Proc(self)])
+        self.accum = ''
+
+
+class Data1Proc(Procedure):
+    def __init__(self, actor: ComputeActor):
+        super().__init__('data1')
+        self._actor = actor
+
+    def call(self, arg: CallArg, __, proxor: IProcessorAPI) -> CallResult:
+        result = '{}*({})'.format(arg, self._actor.accum)
+        self._actor.accum = ''
+        return CallResult(1, 
+                          [Call(result, Address(processor='D', actor='sink'))])
+
+
+class Data2Proc(Procedure):
+    def __init__(self, actor: ComputeActor):
+        super().__init__('data2')
+        self._actor = actor
+
+    def call(self, arg: CallArg, __, proxor: IProcessorAPI) -> CallResult:
+        if self._actor.accum:
+            self._actor.accum = '{} + {}'.format(self._actor.accum, arg)
+        else:
+            self._actor.accum = arg
+        return CallResult()
+
+
+class SinkActor(Actor):
+    def __init__(self):
+        super().__init__('sink', [ConsumeProc()])
+
+
+class ConsumeProc(Procedure):
+    def __init__(self):
+        super().__init__('consume')
+
+    def call(self, arg: CallArg, __, proxor: IProcessorAPI) -> CallResult:
+        logging.warning('Sink received value: {}'.format(arg))
+        return CallResult()
+        #return proxor.exit()
+
+
+def source_A_main(processor: Processor) -> None:
+    processor.add_actor(SourceActor())
+    processor.put_calls([Call(0, Address(actor='source'))])
+
+
+def source_B_main(processor: Processor) -> None:
+    processor.add_actor(DoubleSourceActor())
+    processor.put_calls([Call(None, Address(actor='source'))])
+
+
+def compute_main(processor: Processor) -> None:
+    processor.add_actor(ComputeActor())
+
+
+def sink_main(processor: Processor) -> None:
+    processor.add_actor(SinkActor())
+
+def create_board(log_lvl: str = 'WARNING') -> Board:
+    processor_configs = [
+        ProcessorConfig(name='A', main=source_A_main, log_level=log_lvl), 
+        ProcessorConfig(name='B', main=source_B_main, log_level=log_lvl), 
+        ProcessorConfig(name='C', main=compute_main, log_level=log_lvl), 
+        ProcessorConfig(name='D', main=sink_main, log_level=log_lvl)
+        ]
+    return Board(processor_configs)
+
+
+def run(log_lvl: str = 'WARNING'):
+    print('=== data-race ===')
+    board = create_board(log_lvl)
+    while not board.any_exited():
+        board.step()
+        time.sleep(0.5)  # slow down the simulation artificially
+    board.exit()
+
+
+def description() -> str:
+    return \
+    """
+    An implcitly time-sychronized computation.
+    """
+
+
+def process_args() -> Any:
+    parser = argparse.ArgumentParser(add_help=False)
+    parser.add_argument("--log", default='WARNING')
+    parser.add_argument("--run_count", default=1, type=int)
+    parser.add_argument('-h', '--help', action='help', 
+                        default=argparse.SUPPRESS,
+                        help=description())
+    args = parser.parse_args(sys.argv[1:])
+    logging.basicConfig(format='%(levelname)s:%(message)s',
+                        level=logging.getLevelName(args.log.upper()))
+    random.seed(time.time())
+    return args
+
+
+def main():
+    args = process_args()
+    for _ in range(args.run_count):
+        run(args.log)
+
+
+if __name__ == "__main__":
+    main()
